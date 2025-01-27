0272cbf09fbfd42660064484032558965622056b
magi17 <yen-evade-subsidy@duck.com>
2025-01-27 23:53:01 +0800

    Update random.js

--- pages/api/random.js
+++ pages/api/random.js
@@ -1,17 +1,23 @@
-export default function handler(req, res) {
-    // Array of random responses
-    const responses = [
-        { message: "Hello, world!" },
-        { message: "Welcome to the random API!" },
-        { message: "Next.js is awesome!" },
-        { message: "You got a random message!" },
-        { message: "API responses can be fun!" },
-        { message: "Have a great day!" },
-        { message: "Randomness is exciting!" }
-    ];
+import { NextApiRequest, NextApiResponse } from 'next';
 
-    // Pick a random response
-    const randomResponse = responses[Math.floor(Math.random() * responses.length)];
+interface Response {
+  message: string;
+}
 
-    res.status(200).json(randomResponse);
+export default function handler(req: NextApiRequest, res: NextApiResponse<Response>) {
+  // Array of random responses
+  const responses: Response[] = [
+    { message: "Hello, world!" },
+    { message: "Welcome to the random API!" },
+    { message: "Next.js is awesome!" },
+    { message: "You got a random message!" },
+    { message: "API responses can be fun!" },
+    { message: "Have a great day!" },
+    { message: "Randomness is exciting!" },
+  ];
+
+  // Pick a random response
+  const randomResponse = responses[Math.floor(Math.random() * responses.length)];
+
+  res.status(200).json(randomResponse);
 }
