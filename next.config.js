5d4624f3c836bd7927b13c878237c5dc741d67e2
magi17 <yen-evade-subsidy@duck.com>
2025-02-22 10:14:44 +0000

    c

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    trailingSlash: true, // Ensures proper routing
-    images: {
-      unoptimized: true, // Needed since Cloudflare does not support Next.js image optimization
-    },
-  };
-  
-  module.exports = nextConfig;
+  output: "export",
+};
 
-
-
+module.exports = nextConfig;
